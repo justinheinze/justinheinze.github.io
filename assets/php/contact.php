fcc2dc9538386324538e10cb380ea808eb118144
Justin <justinjheinze@gmail.com>
2016-12-13 21:05:22 -0600

    New Site!

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -6,20 +6,20 @@
     $message = ( $_REQUEST['user-message'] ) ? $_REQUEST['user-message'] : '';
     $status = $_REQUEST['user-status'];
 
-    $to = 'justin@justinheinze.co';
+    $to = 'info@justinheinze.co';
     $message = 'Name: '.$name.'<br /> Email: '.$email.'<br />Message: '.$message;
 
-    $subject = 'Subject';
+    $subject = 'Website Inquiry';
 
-    $headers = "From: You company <".$to.">". "\r\n" .
-                  "Return-Path: You company <info@yousite.com>\r\n".
-                  "Reply-To: You company <".$to.">" . "\r\n" .
+    $headers = "From: Info - Justin Heinze <".$to.">". "\r\n" .
+                  "Return-Path: Info (info@justinheinze.co) <info@justinheinze.co>\r\n".
+                  "Reply-To: Info (info@justinheinze.co)<".$to.">" . "\r\n" .
                   "MIME-Version: 1.0\r\n".
                   "Content-type: text/html; charset=iso-8859-1\r\n".
                   "X-Priority: 3\r\n" .
                   'X-Mailer: PHP/' . phpversion();
 
-    if (filter_var($email, FILTER_VALIDATE_EMAIL) && $status == "yes" ) { // shis line checks that we have a valid email address
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) && $status == "yes" ) { // this line checks that we have a valid email address
         mail($to, $subject, $message, $headers); // this method sends the mail.
         echo "success"; // success message
         exit;
